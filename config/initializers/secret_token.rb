c93265967a2b56819fb5c4b2b02624bb91f0537e
pss <pss@sjea-staging.(none)>
2012-05-09 15:58:40 -0400

    Boilerplate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sjsearch::Application.config.secret_token = 'd6abdd4f01989ce210e9a724d1a685d4c8cf7c45a773572845d9749b6151a43724995b00fb245e82f84d3611a6bded20316a14cce4bd23a3345d85765af4fc04'
