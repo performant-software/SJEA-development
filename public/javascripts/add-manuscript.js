8ba1a942c1171f5874f63358fe73501a54f5eaf1
Dave Goldstein <dave@performantsoftware.com>
2012-05-15 13:27:42 -0400

    Ongoing javascript behavior...

--- public/javascripts/add-manuscript.js
+++ public/javascripts/add-manuscript.js
@@ -1,48 +1,130 @@
 $(document).ready(function() {
 
-	$("#SJA-manuscript").click(function() {
-	    showManuscript( 'SJA-MS');
+    // hide all the sub menu's
+    $( ".sublist").hide()
+
+	$(".mainlist").toggle(function(){
+        $( ".sublist").slideUp(500);
+        $(this).parent().next().slideDown(500);
+		//return false;
+	},
+	function(){
+		$(this).parent().next().slideUp(500);
+		//return false;
 	});
 
-    $("#SJC-manuscript").click(function() {
-        showManuscript( 'SJC-MS');
+	$("#SJA-manuscript").click(function() {
+        highliteElement( $(this) )
+	    showManuscriptContent( 'SJA');
+	});
+
+    $("#SJA-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJA');
     });
+
+
+    $("#SJC-manuscript").click(function() {
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJC');
+    });
+
+    $("#SJC-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJC');
+    });
+
 
     $("#SJD-manuscript").click(function() {
-        showManuscript( 'SJD-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJD');
     });
+
+    $("#SJD-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJD');
+    });
+
 
     $("#SJE-manuscript").click(function() {
-        showManuscript( 'SJE-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJE');
     });
+
+    $("#SJE-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJE');
+    });
+
 
     $("#SJEx-manuscript").click(function() {
-        showManuscript( 'SJEx-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJEx');
     });
+
+    $("#SJEx-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJEx');
+    });
+
 
     $("#SJL-manuscript").click(function() {
-        showManuscript( 'SJL-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJL');
     });
+
+    $("#SJL-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJL');
+    });
+
 
     $("#SJP-manuscript").click(function() {
-        showManuscript( 'SJP-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJP');
     });
+
+    $("#SJP-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJP');
+    });
+
 
     $("#SJU-manuscript").click(function() {
-        showManuscript( 'SJU-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJU');
     });
+
+    $("#SJU-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJU');
+    });
+
 
     $("#SJV-manuscript").click(function() {
-        showManuscript( 'SJV-MS');
+        highliteElement( $(this) )
+        showManuscriptContent( 'SJV');
     });
 
+    $("#SJV-description").click(function() {
+        highliteElement( $(this) )
+        showManuscriptDescription( 'SJV');
+    });
 });
 
-function showManuscript( manuscript ) {
+function highliteElement( element ) {
+    $( ".sublist-item").removeClass( "active");
+    element.addClass( "active");
+}
 
-   var resource_name = manuscript + "-alltags.html"
+function showManuscriptContent( manuscript ) {
+
+   var resource_name = manuscript + "-MS-alltags.html"
+   var main_title = $( "#" + manuscript + "-title").text()
    $("#manuscript").empty();
-   $('#manuscript').load(resource_name, function(response, status, xhr) {
+   $("#active-title").html( "<h2>" + main_title + "</h2>")
+
+   $("#manuscript").load(resource_name, function(response, status, xhr) {
       if (status == "error") {
          var msg = "Sorry but there was an error: ";
          alert(msg + xhr.status + " " + xhr.statusText);
