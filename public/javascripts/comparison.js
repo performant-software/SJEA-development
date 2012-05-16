055c0b7a9129ed3b6619862494d098cb87828065
Dave Goldstein <dave@performantsoftware.com>
2012-05-16 16:46:47 -0400

    Functioning compare using AJAX to get the results from the server

--- public/javascripts/comparison.js
+++ public/javascripts/comparison.js
@@ -2,4 +2,46 @@ $(document).ready(function() {
 
     // hide all the sub menu's
     $( ".sublist").hide()
+
+    $("#previous-button").click(function() {
+        showPreviousComparison();
+    });
+
+    $("#next-button").click(function() {
+        showNextComparison();
+    });
+
 });
+
+function showPreviousComparison( ) {
+
+   var resource_name = $("#previous-page").attr( "href")
+   if (resource_name == null) {
+       resource_name = "HL-0001.html"
+   }
+
+   loadComparison( resource_name );
+}
+
+function showNextComparison( ) {
+
+   var resource_name = $("#next-page").attr( "href");
+   if (resource_name == null) {
+       resource_name = "HL-0001.html"
+   }
+
+   loadComparison( resource_name );
+}
+
+function loadComparison( resource_name ) {
+
+    $("#content-display").empty();
+    $("#compare-page").text( resource_name.slice( 0, 7 ) )
+
+    $("#content-display").load(resource_name, function(response, status, xhr) {
+       if (status == "error") {
+          var msg = "Sorry but there was an error: ";
+          alert(msg + xhr.status + " " + xhr.statusText);
+       }
+    });
+}
