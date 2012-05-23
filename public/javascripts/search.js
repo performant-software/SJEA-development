65d43ad7db51135e2d5064c71ab70eed4226b510
Dave Goldstein <dave@performantsoftware.com>
2012-05-23 15:53:20 -0400

    Placeholder methods for special character buttons

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+    $("#special-button-1").click(function() {
+    });
+
+    $("#special-button-2").click(function() {
+    });
+
+    $("#special-button-3").click(function() {
+    });
+
+    $("#special-button-4").click(function() {
+    });
 });
