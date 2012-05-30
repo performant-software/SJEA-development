8a1dbbd1c03a35c1ce1e7996cfad8789a4286f14
Dave Goldstein <dave@performantsoftware.com>
2012-05-30 12:27:05 -0400

    Special characters inserted into the text search box

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 
     $("#special-button-1").click(function() {
+        addCharacter( '\u210F')
     });
 
     $("#special-button-2").click(function() {
+        addCharacter( '\u019A')
     });
 
     $("#special-button-3").click(function() {
+        addCharacter( '\u0222')
     });
 
     $("#special-button-4").click(function() {
+        addCharacter( '\u04E0')
     });
 });
+
+function addCharacter( character ) {
+    $("#search-text").val( $("#search-text").val() + character )
+}
