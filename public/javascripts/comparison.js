d295180ad91612efc107e740d17be4bc3ae0fd30
Dave Goldstein <dave@performantsoftware.com>
2012-05-30 17:14:38 -0400

    Remove wait overlay for comparison AJAX calls. They are so short

--- public/javascripts/comparison.js
+++ public/javascripts/comparison.js
@@ -42,11 +42,7 @@ function loadComparison( resource_name ) {
     var title = resource_name.replace(/%20/g, " ").replace(/comparisons\//g, "" ).replace(/.html/g, "" );
     $("#compare-page").text( title )
 
-    // nice wait display...
-    showWaitOverlay();
-
     $("#content-display").load(resource_name, function(response, status, xhr) {
-       clearWaitOverlay();
        if (status == "error") {
           var msg = "Sorry but there was an error: ";
           alert(msg + xhr.status + " " + xhr.statusText);
