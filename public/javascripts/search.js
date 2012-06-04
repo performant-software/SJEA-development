47f6b763726cdfd2e0b4501364c2c421e191d414
Dave Goldstein <dave@performantsoftware.com>
2012-06-04 09:26:04 -0400

    Pull focus back to entrty field after inserting a special character

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,21 +3,25 @@ $(document).ready(function() {
     // barred H
     $("#special-button-1").click(function() {
         addCharacter( '\u0127');
+        $("#search-text").focus();
     });
 
     // barred L
     $("#special-button-2").click(function() {
         addCharacter( '\u0142');
+        $("#search-text").focus();
     });
 
     // thorn
     $("#special-button-3").click(function() {
         addCharacter( '\u00DE');
+        $("#search-text").focus();
     });
 
     // yogh
     $("#special-button-4").click(function() {
         addCharacter( '\u021D');
+        $("#search-text").focus();
     });
 
     $("#search-text").focus();
