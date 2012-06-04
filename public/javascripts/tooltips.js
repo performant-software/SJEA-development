3b23a63b4a25973c97f616e0b55ec7f2a4745ec7
Dave Goldstein <dave@performantsoftware.com>
2012-06-04 17:45:31 -0400

    Updated tooltip styling

--- public/javascripts/tooltips.js
+++ public/javascripts/tooltips.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
     $(".standard-tooltip").tooltip({
+        opacity: 0.8,
 		effect: 'fade',
 	 	position: 'top center',
 		offset: [-8, 25],
 		relative: true,
-        delay: 0,
+        delay: 30,
         events: {
 			def: "mouseover, mouseout"
 		}
