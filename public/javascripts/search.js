57ef56f939a1fd9a1ffed3238a77910341851659
Dave Goldstein <dave@performantsoftware.com>
2012-06-07 14:51:10 -0400

    Remove barrd h and barrd l special buttons

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,25 +1,13 @@
 $(document).ready(function() {
 
-    // barred H
-    $("#special-button-1").click(function() {
-        addCharacter( '\u0127');
-        $("#search-text").focus();
-    });
-
-    // barred L
-    $("#special-button-2").click(function() {
-        addCharacter( '\u0142');
-        $("#search-text").focus();
-    });
-
     // thorn
-    $("#special-button-3").click(function() {
+    $("#special-button-1").click(function() {
         addCharacter( '\u00DE');
         $("#search-text").focus();
     });
 
     // yogh
-    $("#special-button-4").click(function() {
+    $("#special-button-2").click(function() {
         addCharacter( '\u021D');
         $("#search-text").focus();
     });
