50c38e8ffb9952d799c33cf5ef48f22c6970b92c
Dave Goldstein <dave@performantsoftware.com>
2012-06-12 09:17:54 -0400

    Big styling session

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-    // disguise this as we dont need it
-    $("#content-controls").css("background-color", "#F0E7CF");
-
     // thorn
     $("#special-button-1").click(function() {
         addCharacter( '\u00DE');
