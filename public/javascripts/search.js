2947c5db3d3dd2f4e45cdee169bd38ba00b402d8
Dave Goldstein <dave@performantsoftware.com>
2012-06-12 13:47:02 -0400

    Ensure the cursor lands at the end of the text entry field

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -4,12 +4,14 @@ $(document).ready(function() {
     $("#special-button-1").click(function() {
         addCharacter( '\u00DE');
         $("#search-text").focus();
+        setCaretPosition( "search-text", $("#search-text").val( ).length );
     });
 
     // yogh
     $("#special-button-2").click(function() {
         addCharacter( '\u021D');
         $("#search-text").focus();
+        setCaretPosition( "search-text", $("#search-text").val( ).length );
     });
 
     $("#search-text").focus();
@@ -18,4 +20,25 @@ $(document).ready(function() {
 
 function addCharacter( character ) {
     $("#search-text").val( $("#search-text").val() + character )
+}
+
+// grabbed this from a google search...
+function setCaretPosition(elemId, caretPos) {
+    var elem = document.getElementById(elemId);
+
+    if(elem != null) {
+        if(elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if(elem.selectionStart) {
+                elem.focus();
+                elem.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                elem.focus();
+        }
+    }
 }
