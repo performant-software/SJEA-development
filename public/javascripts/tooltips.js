f4b69506e4e3ed469cbc442eecd187b2e32fc8c7
Dave Goldstein <dave@performantsoftware.com>
2012-07-05 17:39:10 -0400

    Use different jquery tooltip plugin

--- public/javascripts/tooltips.js
+++ public/javascripts/tooltips.js
@@ -1,19 +1,10 @@
-$(document).ready(function() {
-   decorateTooltips( );
-});
-
 function decorateTooltips( ) {
 
-    $(".standard-tooltip").tooltip({
-        opacity: 0.8,
-		fade: 250,
-	 	position: 'top center',
-        offset: [-20,-75],
-		relative: true,
-        delay: 0,
-        events: {
-			def: "mouseover, mouseout"
-		}
-	});
+    $(".standard-tooltip").qtip( {
+        position: { adjust: { screen: true } },
+        style: { border: { color: '#000', width: 1, radius: 3 } },
+        show: { delay: 150, effect: { type: 'slide' } },
+        hide: { delay: 250, effect: { type: 'slide' } }
+    });
 
 }
